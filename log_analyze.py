e4e704db44a68f95bb4391ff0cfbdf675bc478f4
fariezfik <fariezfikhi.ff@gmail.com>
2026-03-11 22:43:41 +0800

    "update log_analyzer to use iterative loops and status code counters"

--- log_analyze.py
+++ log_analyze.py
@@ -1,4 +1,16 @@
-print("hello world")
-
 f=open("/Users/fariezfikhi/repos/app-support-portfolio/access.log")
-print(f.read())
+
+count404 =0
+count500 = 0
+for line in f:
+    if "404" in line:
+        count404+= 1
+    if "500 " in line:
+        count500+=1
+
+print("Total Error 404 :",count404)
+print("Total Error 500 :",count500)
+
+
+
+f.close()
